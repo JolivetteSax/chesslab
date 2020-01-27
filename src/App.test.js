d78c59990666cf2397cdd473c8599807f936f614
JolivetteSax <erikjsax@gmail.com>
2020-01-27 15:48:24 -0500

    added simple unit test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const queen = getByText(/wq/i);
+  expect(queen).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  const obj = new App();
+  let id = obj.getId(0, 0);
+  expect(id).toEqual('A8');
+  id = obj.getId(7, 7);
+  expect(id).toEqual('H1');
+  let [x,y] = obj.decodePosition(id);
+  expect(x).toEqual(7);
+  expect(y).toEqual(7);
+});
+
